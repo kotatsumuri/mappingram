f52afe22c55ac1cec9f777714473156ba4700a19
wakadai <86338640+wakadai@users.noreply.github.com>
2021-06-30 14:18:05 +0900

    login new

--- mappingram/src/views/login.vue
+++ mappingram/src/views/login.vue
@@ -1,13 +1,38 @@
 <template>
     <div class = "login">
+        <v-container>
+
         <div class = "caption text-center title">
             <font size="6"><i><b>mappingram</b></i></font>
         </div>
-        <br><br>
-        <dir class = "text-center">
-            <font size="2">メールアドレス</font>
-        </dir>
+        <br><br><br>
+        <v-flex xs12>
+            <v-flex xs10 sm6 md4 class="select">
+          <v-text-field
+            label="maill adress"
+            outlined
+          ></v-text-field>
+            </v-flex>
+        </v-flex>
+        <v-flex xs12>
+            <v-flex xs10 sm6 md4 class="select">
+          <v-text-field
+            label="your password"
+            outlined
+          ></v-text-field>
+            </v-flex>
+        </v-flex>
 
+        <v-flex xs12>
+            <div class="text-center">
+
+
+          <v-btn class = "ma-3">login</v-btn>
+            </div>
+        </v-flex>
+
+        
+        </v-container>
     </div>
 </template>
 
@@ -21,6 +46,9 @@
 <style scoped>
     .title{
         margin-top: 20vh;
+    }
+    .select{
+    margin: auto;
     }
 
 </style>
